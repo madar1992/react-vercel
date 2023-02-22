7be2d8770d9970eea8bc581b19201e91e8cd6ac6
madar1992 <96854154+madar1992@users.noreply.github.com>
2023-02-22 21:34:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-   
+        <form>
+  <label for="field1">Field 1:</label>
+  <input type="text" id="field1" name="field1"><br>
+
+  <label for="field2">Field 2:</label>
+  <input type="text" id="field2" name="field2"><br>
+
+  <input type="submit" value="Submit">
+</form>
         <p>
           I have just made some changes to this application.
         </p>
